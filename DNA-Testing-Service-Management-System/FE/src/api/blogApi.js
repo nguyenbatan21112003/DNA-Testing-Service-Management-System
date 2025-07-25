c6f320c71b3f276aff63f93608e08880c47bb12b
Nguyễn Đức Thiên Ân-SE182633 <133340282+ThienAnaka@users.noreply.github.com>
2025-07-25 20:57:04 +0700

    Delete DNA-Testing-Service-Management-System directory

--- DNA-Testing-Service-Management-System/FE/src/api/blogApi.js
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "./axiosInstance";
-
-const blogApi = {
-    getBlogs: () => axiosInstance.get(`/user/blogPost`),
-    getBlogBySlug: (slug) => axiosInstance.get(`/user/blogPost/${slug}`),
-}
-
-export default blogApi;
